ded773065d764bd75ecd13dca78bae1a8d5e4f20
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-11-23 12:25:31 +0530

    inputlearn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule],
+  imports: [CommonModule ,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'sample';
   boo = true;
-  log() {
-    this.boo = !this.boo;
-  }
+  
 }
