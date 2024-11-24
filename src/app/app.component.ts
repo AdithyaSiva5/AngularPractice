c81938f0eb74719cf7026b6e4aaa967ad68e89e9
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-11-24 07:00:34 +0530

    routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,NgOptimizedImage],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -11,5 +12,6 @@ export class AppComponent {
   isLoading: boolean = true; // Initially, content is loading
   contentLoaded: boolean = false; // Tracks if the content is ready
   title = 'sample';
-  imglogo = 'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75';
+  imglogo =
+    'https://next-images.123rf.com/index/_next/image/?url=https://assets-cdn.123rf.com/index/static/assets/top-section-bg.jpeg&w=3840&q=75';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+  },
+];
