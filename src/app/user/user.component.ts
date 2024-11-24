a203f4f77d887577c92298804f738468ba805634
AdithyaSiva5 <adithyasiva111@gmail.com>
2024-11-24 08:31:16 +0530

    pipes

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,27 +11,22 @@ import {
 import { ReactiveFormsModule, Validator, Validators } from '@angular/forms';
 import { FormsModule, FormControl, FormGroup } from '@angular/forms';
 import { UserService } from '../services/UserService';
+import { DecimalPipe, DatePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    DecimalPipe,
+    DatePipe,
+    CurrencyPipe,
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-export class UserComponent implements OnInit {
-  userInfo: any;
-  carno: number = 2;
-  car: string | undefined;
-  userService;
-  carService = inject(UserService);
-
-  constructor(private injector: Injector) {
-    // Manually retrieve the service using the Injector
-    this.userService = this.injector.get(UserService);
-  }
-
-  ngOnInit(): void {
-    this.userInfo = this.carService.getCars();
-    this.car = this.userService.getCar(this.carno);
-  }
+export class UserComponent {
+  num = 103.1234;
+  birthday = new Date(2023, 3, 2);
+  cost = 4560.34;
 }
